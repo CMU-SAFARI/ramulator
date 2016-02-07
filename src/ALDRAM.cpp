6051c61916ca31fc52ead0c57efb88a2bdbb495d
Tianshi Li <9413fi@gmail.com>
2016-02-06 21:44:24 -0500

    bugfix: missing init_rowopen in ALDRAM and WideIO2

--- src/ALDRAM.cpp
+++ src/ALDRAM.cpp
@@ -38,6 +38,7 @@ ALDRAM::ALDRAM(Org org, Speed speed) :
     init_speed();
     init_prereq();
     init_rowhit(); // SAUGATA: added row hit function
+    init_rowopen();
     init_lambda();
     init_timing(speed_table[int(Temp::HOT)][int(speed)]);
     temperature = Temp::COLD;
@@ -182,6 +183,19 @@ void ALDRAM::init_rowhit()
     rowhit[int(Level::Bank)][int(Command::WR)] = rowhit[int(Level::Bank)][int(Command::RD)];
 }
 
+void ALDRAM::init_rowopen()
+{
+    // RD
+    rowopen[int(Level::Bank)][int(Command::RD)] = [] (DRAM<ALDRAM>* node, Command cmd, int id) {
+        switch (int(node->state)) {
+            case int(State::Closed): return false;
+            case int(State::Opened): return true;
+            default: assert(false);
+        }};
+
+    // WR
+    rowopen[int(Level::Bank)][int(Command::WR)] = rowopen[int(Level::Bank)][int(Command::RD)];
+}
 
 void ALDRAM::init_lambda()
 {
