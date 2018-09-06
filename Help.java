ca961daf178b29a5fac5baff728b707aba31a6fe
Nisa Bostancı <nisabstnc@gmail.com>
2018-09-06 20:15:59 +0300

    minor changes

--- Help.java
+++ Help.java
@@ -8,19 +8,27 @@ import java.io.PrintWriter;
 public class Help{
   public static ArrayList<String> fileList = new ArrayList<String>();
 	public static void main(String[] args) {
+    if( args.length<1) {
+      System.out.println("to get results give arg. 0, to get slowdown.csv give arg. 1.");
+      System.exit(0);
+    }
     if(args[0].equals("0")) {
       System.out.println(args[0]);
       getResults(0);
-      getResults(1);
+      //getResults(1);
     }
     else {
       try{
-        PrintWriter pw = new PrintWriter(new File("slowdown.csv"));
-        pw.write("trace,slowdown\n");
+        PrintWriter pw = new PrintWriter(new File("slowdown2-0L3.csv"));
+        pw.write("trace,slowdown,stt,ddr3\n");
         initStatList();
         for (String trace : fileList) {
-          pw.write(trace.substring(trace.indexOf(".")+1) + "," + getSlowdown(trace) + "\n");
-          System.out.println(trace + "," + getSlowdown(trace));
+          String txt = trace.substring(trace.indexOf(".")+1) + ","
+          + getSlowdown(trace) + "," + getCPI("./results2-0L3/"
+          + trace + "_stt.stats") + "," + getCPI("./results2-0L3/"
+          + trace + "_ddr3.stats") +"\n";
+          pw.write(txt);
+          System.out.println(txt);
         }
         pw.close();
       }
@@ -42,10 +50,10 @@ public class Help{
     }
   }
   public static double getSlowdown(String traceName){
-    String stt_name = "./results/" + traceName + "_stt.stats";
-    String ddr3_name = "./results/" + traceName + "_ddr3.stats";
-    int stt = getTotalActiveCycles(stt_name);
-    int ddr3 = getTotalActiveCycles(ddr3_name);
+    String stt_name = "./results2-0L3/" + traceName + "_stt.stats";
+    String ddr3_name = "./results2-0L3/" + traceName + "_ddr3.stats";
+    double stt = getCPI(stt_name);
+    double ddr3 = getCPI(ddr3_name);
     double slowdown = ((stt - ddr3) *100.0 / ddr3);
     return slowdown;
   }
@@ -57,19 +65,17 @@ public class Help{
       for (File child : directoryListing) {
       //  System.out.println(child.getName());
         if(child.getName().indexOf(".gz")<0) {
-          System.out.println("running.");
           fileList.add(child.getName());
 
           String config = "";
           String statfile = "";
           if (cfg==0) {
             config = "configs/STTMRAM-config.cfg";
-            statfile = "./results/" + child.getName() + "_stt.stats";
+            statfile = "./results2-0L3/" + child.getName() + "_stt.stats";
           }
           else {
-            System.out.println("here");
             config = "configs/DDR3-config.cfg";
-            statfile = "./results/" + child.getName() + "_ddr3.stats";
+            statfile = "./results2-0L3/" + child.getName() + "_ddr3.stats";
           }
           String mode = "--mode=cpu";
           String tracePath = child.getAbsolutePath();
@@ -86,30 +92,39 @@ public class Help{
       System.out.println("oops");
     }
   }
-  public static int getTotalActiveCycles(String filename){
+  public static double getCPI(String filename){
     try {
 			File file = new File(filename);
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
 			String line;
+      long cycles = 0;
+      long insts = 0;
 			while ((line = bufferedReader.readLine()) != null) {
-        if (line.indexOf("ramulator_active_cycles")>=0) {
+        if (line.indexOf("ramulator.cpu_cycles")>=0) {
           StringTokenizer t = new StringTokenizer(line, " ");
           int i = 0;
           while(t.hasMoreTokens()){
             String token = t.nextToken();
-            if(isNumber(token)) {
-              //System.out.println("\t" + token);
-              return Integer.parseInt(token);
-              }
-            else if(i == 0)
-              //System.out.print(filename + "\t" + token);
+            if(isNumber(token))
+              cycles = Integer.parseInt(token);
+            i++;
+          }
+        }
+        else if (line.indexOf("ramulator.cpu_instructions_core_0")>=0){
+          StringTokenizer t = new StringTokenizer(line, " ");
+          int i = 0;
+          while(t.hasMoreTokens()){
+            String token = t.nextToken();
+            if(isNumber(token))
+              insts = Integer.parseInt(token);
             i++;
           }
         }
 			}
 			fileReader.close();
+      return cycles / ((double)insts);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/STTMRAM.cpp
+++ src/STTMRAM.cpp
@@ -54,33 +54,18 @@ void STTMRAM::init_speed()
     // nRRD, nFAW
     int page = (org_entry.dq * org_entry.count[int(Level::Column)]) >> 13;
     switch (speed_entry.rate) {
-        //case 800:  speed_entry.nRRD = (page==1) ? 4 : 4; speed_entry.nFAW = (page==1) ? 16 : 20; break;
-      //  case 1066: speed_entry.nRRD = (page==1) ? 4 : 6; speed_entry.nFAW = (page==1) ? 20 : 27; break;
-      //  case 1333: speed_entry.nRRD = (page==1) ? 4 : 5; speed_entry.nFAW = (page==1) ? 20 : 30; break;
           case 1600: speed_entry.nRRD = ceil(speed_entry.nRRD  * ((page==1) ? 5 : 6) /10.0); speed_entry.nFAW = ceil(speed_entry.nFAW *((page==1) ? 24 : 32)/10.0); break;
-        //case 1866: speed_entry.nRRD = (page==1) ? 5 : 6; speed_entry.nFAW = (page==1) ? 26 : 33; break;
-        //case 2133: speed_entry.nRRD = (page==1) ? 5 : 6; speed_entry.nFAW = (page==1) ? 27 : 34; break;
         default: assert(false);
     }
 
     // nRFC, nXS
     int chip = org_entry.size;
     switch (speed_entry.rate) {
-        //case 800:  speed_entry.nRFC = (chip==512) ? 36  : (chip==1<<10) ? 44  : (chip==1<<11) ? 64  : (chip==1<<12) ? 104 : 140; break;
-        //case 1066: speed_entry.nRFC = (chip==512) ? 48  : (chip==1<<10) ? 59  : (chip==1<<11) ? 86  : (chip==1<<12) ? 139 : 187; break;
-        //case 1333: speed_entry.nRFC = (chip==512) ? 60  : (chip==1<<10) ? 74  : (chip==1<<11) ? 107 : (chip==1<<12) ? 174 : 234; break;
         case 1600: speed_entry.nRFC = 1; break;
-        //case 1866: speed_entry.nRFC = (chip==512) ? 84  : (chip==1<<10) ? 103 : (chip==1<<11) ? 150 : (chip==1<<12) ? 243 : 327; break;
-        //case 2133: speed_entry.nRFC = (chip==512) ? 96  : (chip==1<<10) ? 118 : (chip==1<<11) ? 171 : (chip==1<<12) ? 278 : 374; break;
         default: assert(false);
     }
     switch (speed_entry.rate) {
-        //case 800:  speed_entry.nXS  = (chip==512) ? 40  : (chip==1<<10) ? 48  : (chip==1<<11) ? 68  : (chip==1<<12) ? 108 : 144; break;
-        //case 1066: speed_entry.nXS  = (chip==512) ? 54  : (chip==1<<10) ? 64  : (chip==1<<11) ? 91  : (chip==1<<12) ? 144 : 192; break;
-      //  case 1333: speed_entry.nXS  = (chip==512) ? 67  : (chip==1<<10) ? 80  : (chip==1<<11) ? 114 : (chip==1<<12) ? 180 : 240; break;
           case 1600: speed_entry.nXS  = ceil(speed_entry.nXS * ((chip==512) ? 80  : (chip==1<<10) ? 96  : (chip==1<<11) ? 136 : (chip==1<<12) ? 216 : 288) /10.0); break;
-      //  case 1866: speed_entry.nXS  = (chip==512) ? 94  : (chip==1<<10) ? 112 : (chip==1<<11) ? 159 : (chip==1<<12) ? 252 : 336; break;
-        //case 2133: speed_entry.nXS  = (chip==512) ? 107 : (chip==1<<10) ? 128 : (chip==1<<11) ? 182 : (chip==1<<12) ? 288 : 384; break;
         default: assert(false);
     }
 }
