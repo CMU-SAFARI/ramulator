3582c445bab9b6be52e126338f8ca676f4fc2e34
Shivani Prasad <32909787+metafly@users.noreply.github.com>
2019-09-12 04:26:00 -0400

    Shivani updates (#76)
    * Fixed a bug which made FCFS Scheduler fail an assert in DDR3 and DDR4. Also made Scheduler.h more readable
    
    * 75% documentation done for src/scheduler.h
    
    * Added Documentation to Scheduler.h

--- src/Scheduler.h
+++ src/Scheduler.h
@@ -1,3 +1,48 @@
+/***************************** SCHEDULER.H ***********************************
+- SAFARI GROUP
+
+This file contains the different scheduling policies and row policies that the 
+memory controller can use to schedule requests.
+
+Current Memory Scheduling Policies:
+
+1) FCFS - First Come First Serve
+        This scheduling policy schedules memory requests chronologically
+
+2) FRFCFS - Frist Ready First Come First Serve
+        This scheduling policy first checks if a request is READY(meets all 
+        timing parameters), if yes then it is prioritized. If multiple requests
+        are ready, they they are scheduled chronologically. Otherwise, it 
+        behaves the same way as FCFS. 
+
+3) FRFCFS_Cap - First Ready First Come First Serve Cap
+       This scheduling policy behaves the same way as FRFCS, except that it has
+       a cap on the number of hits you can get in a certain row. The CAP VALUE
+       can be altered by changing the number for the "cap" variable in 
+       line number 76. 
+
+4) FRFCFS_PriorHit - First Ready First Come First Serve Prioritize Hits
+       This scheduling policy behaves the same way as FRFCFS, except that it
+       prioritizes row hits more than readiness. 
+
+You can select which scheduler you want to use by changing the value of 
+"type" variable on line number 74.
+
+                _______________________________________
+
+Current Row Policies:
+
+1) Closed   - Precharges a row as soon as there are no pending references to 
+              the active row.
+2) ClosedAP - Closed Auto Precharge
+3) Opened   - Precharges a row only if there are pending references to 
+              other rows.
+4) Timeout  - Precharges a row after X time if there are no pending references.
+              'X' time can be changed by changing the variable timeout 
+              on line number 221
+
+*****************************************************************************/
+
 #ifndef __SCHEDULER_H
 #define __SCHEDULER_H
 
@@ -26,10 +71,9 @@ public:
 
     enum class Type {
         FCFS, FRFCFS, FRFCFS_Cap, FRFCFS_PriorHit, MAX
-    } type = Type::FRFCFS_Cap;
-    //} type = Type::FCFS;
+    } type = Type::FRFCFS_Cap; //Change this line to change scheduling policy
 
-    long cap = 16;
+    long cap = 16; //Change this line to change cap
 
     Scheduler(Controller<T>* ctrl) : ctrl(ctrl) {}
 
@@ -37,18 +81,24 @@ public:
     {
         // TODO make the decision at compile time
         if (type != Type::FRFCFS_PriorHit) {
+            //If queue is empty, return end of queue
             if (!q.size())
                 return q.end();
 
+            //Else return based on the policy
             auto head = q.begin();
             for (auto itr = next(q.begin(), 1); itr != q.end(); itr++)
                 head = compare[int(type)](head, itr);
 
             return head;
-        } else {
+        } 
+        else { //Code to get around edge cases for FRFCFS_PriorHit
+            
+       //If queue is empty, return end of queue
             if (!q.size())
                 return q.end();
 
+       //Else return based on FRFCFS_PriorHit Scheduling Policy
             auto head = q.begin();
             for (auto itr = next(q.begin(), 1); itr != q.end(); itr++) {
                 head = compare[int(Type::FRFCFS_PriorHit)](head, itr);
@@ -104,6 +154,7 @@ public:
         }
     }
 
+//Compare functions for each memory schedulers
 private:
     typedef list<Request>::iterator ReqIter;
     function<ReqIter(ReqIter, ReqIter)> compare[int(Type::MAX)] = {
@@ -156,6 +207,7 @@ private:
 };
 
 
+// Row Precharge Policy
 template <typename T>
 class RowPolicy
 {
